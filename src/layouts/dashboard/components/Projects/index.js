cfaf57b254ec97f34d417d57035bbbe5075c689d
Chancellor Daniels <32651476+daniels1003@users.noreply.github.com>
2024-01-10 14:32:36 -0600

    Update index.js

--- src/layouts/dashboard/components/Projects/index.js
+++ src/layouts/dashboard/components/Projects/index.js
@@ -1,111 +1,251 @@
-/*!
-
-=========================================================
-* Vision UI Free React - v1.0.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/vision-ui-free-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com/)
-* Licensed under MIT (https://github.com/creativetimofficial/vision-ui-free-react/blob/master LICENSE.md)
-
-* Design and Coded by Simmmple & Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-
-import { useState } from "react";
-
 // @mui material components
-import Card from "@mui/material/Card";
-import Icon from "@mui/material/Icon";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import { BsCheckCircleFill } from "react-icons/bs";
+import Tooltip from "@mui/material/Tooltip";
 
 // Vision UI Dashboard React components
 import VuiBox from "components/VuiBox";
 import VuiTypography from "components/VuiTypography";
+import VuiAvatar from "components/VuiAvatar";
+import VuiProgress from "components/VuiProgress";
 
-// Vision UI Dashboard Materail-UI example components
-import Table from "examples/Tables/Table";
+// Images
+import AdobeXD from "examples/Icons/AdobeXD";
+import Atlassian from "examples/Icons/Atlassian";
+import Slack from "examples/Icons/Slack";
+import Spotify from "examples/Icons/Spotify";
+import Jira from "examples/Icons/Jira";
+import Invision from "examples/Icons/Invision";
+import avatar1 from "assets/images/avatar1.png";
+import avatar2 from "assets/images/avatar2.png";
+import avatar3 from "assets/images/avatar3.png";
+import avatar4 from "assets/images/avatar4.png";
 
-// Data
-import data from "layouts/dashboard/components/Projects/data";
+export default function data() {
+  const avatars = (members) =>
+    members.map(([image, name]) => (
+      <Tooltip key={name} title={name} placeholder="bottom">
+        <VuiAvatar
+          src={image}
+          alt="name"
+          size="xs"
+          sx={{
+            border: ({ borders: { borderWidth }, palette: { dark } }) =>
+              `${borderWidth[2]} solid ${dark.focus}`,
+            cursor: "pointer",
+            position: "relative",
 
-function Projects() {
-  const { columns, rows } = data();
-  const [menu, setMenu] = useState(null);
+            "&:not(:first-of-type)": {
+              ml: -1.25,
+            },
 
-  const openMenu = ({ currentTarget }) => setMenu(currentTarget);
-  const closeMenu = () => setMenu(null);
+            "&:hover, &:focus": {
+              zIndex: "10",
+            },
+          }}
+        />
+      </Tooltip>
+    ));
 
-  const renderMenu = (
-    <Menu
-      id="simple-menu"
-      anchorEl={menu}
-      anchorOrigin={{
-        vertical: "top",
-        horizontal: "left",
-      }}
-      transformOrigin={{
-        vertical: "top",
-        horizontal: "right",
-      }}
-      open={Boolean(menu)}
-      onClose={closeMenu}
-    >
-      <MenuItem onClick={closeMenu}>Action</MenuItem>
-      <MenuItem onClick={closeMenu}>Another action</MenuItem>
-      <MenuItem onClick={closeMenu}>Something else</MenuItem>
-    </Menu>
-  );
+  return {
+    columns: [
+      { name: "programs", align: "left" },
+      { name: "members", align: "left" },
+      { name: "budget", align: "center" },
+      { name: "completion", align: "center" },
+    ],
 
-  return (
-    <Card
-      sx={{
-        height: "100% !important",
-      }}
-    >
-      <VuiBox display="flex" justifyContent="space-between" alignItems="center" mb="32px">
-        <VuiBox mb="auto">
-          <VuiTypography color="white" variant="lg" mb="6px" gutterBottom>
-            Projects
-          </VuiTypography>
-          <VuiBox display="flex" alignItems="center" lineHeight={0}>
-            <BsCheckCircleFill color="green" size="15px" />
-            <VuiTypography variant="button" fontWeight="regular" color="text" ml="5px">
-              &nbsp;<strong>30 done</strong> this month
+    rows: [
+      {
+        programs: (
+          <VuiBox display="flex" alignItems="center">
+            <AdobeXD size="20px" />
+            <VuiTypography pl="16px" color="white" variant="button" fontWeight="medium">
+              GFE
             </VuiTypography>
           </VuiBox>
-        </VuiBox>
-        <VuiBox color="text" px={2}>
-          <Icon sx={{ cursor: "pointer", fontWeight: "bold" }} fontSize="small" onClick={openMenu}>
-            more_vert
-          </Icon>
-        </VuiBox>
-        {renderMenu}
-      </VuiBox>
-      <VuiBox
-        sx={{
-          "& th": {
-            borderBottom: ({ borders: { borderWidth }, palette: { grey } }) =>
-              `${borderWidth[1]} solid ${grey[700]}`,
-          },
-          "& .MuiTableRow-root:not(:last-child)": {
-            "& td": {
-              borderBottom: ({ borders: { borderWidth }, palette: { grey } }) =>
-                `${borderWidth[1]} solid ${grey[700]}`,
-            },
-          },
-        }}
-      >
-        <Table columns={columns} rows={rows} />
-      </VuiBox>
-    </Card>
-  );
+        ),
+        members: (
+          <VuiBox display="flex" py={1}>
+            {avatars([
+              [avatar1, "Chancellor Daniels"],
+              [avatar2, "Aaron Haerle"],
+              [avatar3, "Alexander Smith"],
+              [avatar4, "Jessica Doe"],
+            ])}
+          </VuiBox>
+        ),
+        budget: (
+          <VuiTypography variant="button" color="white" fontWeight="bold">
+            $14,000
+          </VuiTypography>
+        ),
+        completion: (
+          <VuiBox width="8rem" textAlign="left">
+            <VuiTypography color="white" variant="button" fontWeight="bold">
+              78%
+            </VuiTypography>
+            <VuiProgress value={60} color="info" label={false} sx={{ background: "#2D2E5F" }} />
+          </VuiBox>
+        ),
+      },
+      {
+        programs: (
+          <VuiBox display="flex" alignItems="center">
+            <Atlassian size="20px" />
+            <VuiTypography pl="16px" color="white" variant="button" fontWeight="medium">
+              P3R
+            </VuiTypography>
+          </VuiBox>
+        ),
+        members: (
+          <VuiBox display="flex" py={1}>
+            {avatars([
+              [avatar2, "Romina Hadid"],
+              [avatar4, "Jessica Doe"],
+            ])}
+          </VuiBox>
+        ),
+        budget: (
+          <VuiTypography variant="button" color="white" fontWeight="bold">
+            $3,000
+          </VuiTypography>
+        ),
+        completion: (
+          <VuiBox width="8rem" textAlign="left">
+            <VuiTypography color="white" variant="button" fontWeight="bold">
+              41%
+            </VuiTypography>
+            <VuiProgress value={10} color="info" label={false} sx={{ background: "#2D2E5F" }} />
+          </VuiBox>
+        ),
+      },
+      {
+        programs: (
+          <VuiBox display="flex" alignItems="center">
+            <Slack size="20px" />
+            <VuiTypography pl="16px" color="white" variant="button" fontWeight="medium">
+              UK-E7
+            </VuiTypography>
+          </VuiBox>
+        ),
+        members: (
+          <VuiBox display="flex" py={1}>
+            {avatars([
+              [avatar1, "Ryan Tompson"],
+              [avatar3, "Alexander Smith"],
+            ])}
+          </VuiBox>
+        ),
+        budget: (
+          <VuiTypography variant="button" color="white" fontWeight="bold">
+            25,600
+          </VuiTypography>
+        ),
+        completion: (
+          <VuiBox width="8rem" textAlign="left">
+            <VuiTypography color="white" variant="button" fontWeight="bold">
+              84%
+            </VuiTypography>
+            <VuiProgress value={100} color="info" label={false} sx={{ background: "#2D2E5F" }} />
+          </VuiBox>
+        ),
+      },
+      {
+        programs: (
+          <VuiBox display="flex" alignItems="center">
+            <Spotify size="20px" />
+            <VuiTypography pl="16px" color="white" variant="button" fontWeight="medium">
+              E4B
+            </VuiTypography>
+          </VuiBox>
+        ),
+        members: (
+          <VuiBox display="flex" py={1}>
+            {avatars([
+              [avatar4, "Mario Franklin"],
+              [avatar3, "Alexander Smith"],
+              [avatar2, "Romina Hadid"],
+              [avatar1, "Ryan Tompson"],
+            ])}
+          </VuiBox>
+        ),
+        budget: (
+          <VuiTypography variant="button" color="white" fontWeight="bold">
+            $20,500
+          </VuiTypography>
+        ),
+        completion: (
+          <VuiBox width="8rem" textAlign="left">
+            <VuiTypography color="white" variant="button" fontWeight="bold">
+              67%
+            </VuiTypography>
+            <VuiProgress value={100} color="info" label={false} sx={{ background: "#2D2E5F" }} />
+          </VuiBox>
+        ),
+      },
+      
+    ],
+  };
 }
 
-export default Projects;
+/*
+
+{
+  companies: (
+    <VuiBox display="flex" alignItems="center">
+      <Jira size="20px" />
+      <VuiTypography pl="16px" color="white" variant="button" fontWeight="medium">
+        Add the New Pricing Page
+      </VuiTypography>
+    </VuiBox>
+  ),
+  members: (
+    <VuiBox display="flex" py={1}>
+      {avatars([[avatar4, "Jessica Doe"]])}
+    </VuiBox>
+  ),
+  budget: (
+    <VuiTypography variant="button" color="white" fontWeight="bold">
+      $500
+    </VuiTypography>
+  ),
+  completion: (
+    <VuiBox width="8rem" textAlign="left">
+      <VuiTypography color="white" variant="button" fontWeight="bold">
+        25%
+      </VuiTypography>
+      <VuiProgress value={25} color="info" label={false} sx={{ background: "#2D2E5F" }} />
+    </VuiBox>
+  ),
+},
+{
+  companies: (
+    <VuiBox display="flex" alignItems="center">
+      <Invision size="20px" />
+      <VuiTypography pl="16px" color="white" variant="button" fontWeight="medium">
+        Redesign New Online Shop
+      </VuiTypography>
+    </VuiBox>
+  ),
+  members: (
+    <VuiBox display="flex" py={1}>
+      {avatars([
+        [avatar1, "Ryan Tompson"],
+        [avatar4, "Jessica Doe"],
+      ])}
+    </VuiBox>
+  ),
+  budget: (
+    <VuiTypography variant="button" color="white" fontWeight="bold">
+      $2,000
+    </VuiTypography>
+  ),
+  completion: (
+    <VuiBox width="8rem" textAlign="left">
+      <VuiTypography color="white" variant="button" fontWeight="bold">
+        40%
+      </VuiTypography>
+      <VuiProgress value={40} color="info" label={false} sx={{ background: "#2D2E5F" }} />
+    </VuiBox>
+  ),
+},
+*/
