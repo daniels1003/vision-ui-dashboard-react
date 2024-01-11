0a3730e2117b01eafeceec1f4552337516a6144c
Chancellor Daniels <32651476+daniels1003@users.noreply.github.com>
2024-01-10 18:14:11 -0600

    Update index.js

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -230,8 +230,17 @@ function Dashboard() {
               </Card>
             </Grid>
           </Grid>
-       
-      
+       </VuiBox>
+       <Grid container spacing={3} direction="row" justifyContent="center" alignItems="stretch">
+          <Grid item xs={12} md={6} lg={8}>
+            <Projects />
+          </Grid>
+          <Grid item xs={12} md={6} lg={4}>
+            <OrderOverview />
+          </Grid>
+        </Grid>
+      </VuiBox>
+      <Footer />
     </DashboardLayout>
   );
 }
