6e5e07bdbb9de5fd0c2f7bef69d6779da0319d96
Chancellor Daniels <32651476+daniels1003@users.noreply.github.com>
2024-06-05 21:17:49 -0500

    Update index.js
    removing bs from sign in landing page

--- src/layouts/authentication/sign-in/index.js
+++ src/layouts/authentication/sign-in/index.js
@@ -50,8 +50,8 @@ function SignIn() {
       title="Nice to see you!"
       color="white"
       description="Enter your email and password to sign in"
-      premotto="INSPIRED BY THE FUTURE:"
-      motto="THE VISION UI DASHBOARD"
+      premotto="INSPIRing:"
+      motto="GFE Sign In"
       image={bgSignIn}
     >
       <VuiBox component="form" role="form">
